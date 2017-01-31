430ab53a5a7e4cf0037e12f7be4d1e38a1b4afa3
chaitanyamrp <chaitanyamrp@gmail.com>
2017-01-31 22:40:13 +0530

    looping threads package

---
+++ first_package/first.py
@@ -0,0 +1,2 @@
+print "this is awesome!"
+print "hi there!"
---
+++ looping_threads/threadClass.py
@@ -0,0 +1,27 @@
+import threading
+import Queue
+
+q = Queue.Queue()
+
+class listener(object):
+    def __init__(self):
+        thread = threading.Thread(target=self.loop)
+        thread.daemon = True
+        thread.start()
+
+    def loop(self):
+        for i in xrange(0,13):
+            q.put(i)
+            if i==10:
+                break
+
+class ui(object):
+    def __init__(self):
+        listener()
+        while True:
+            item = q.get()
+            print item
+            if item == 10:
+                break
+
+ui()
